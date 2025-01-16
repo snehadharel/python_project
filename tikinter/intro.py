a3891091be862a5a1b6f7323684f380b088629a8
snehadharel26 <snehadharel26@gmail.com>
2025-01-16 08:44:00 +0545

    commit

---
+++ tikinter/intro.py
@@ -0,0 +1,15 @@
+from tkinter import *
+root=Tk()
+root.geometry("300x400")
+# width x height
+
+root.minsize(200,300)
+root.maxsize(400,500)
+root.title("hello")
+Label(root,text= "hello everyone").pack()
+Label(root,text="ethical 35").pack(side=BOTTOM)
+Label(root,text="sneha dharel").pack()
+
+root.mainloop()
+
+
